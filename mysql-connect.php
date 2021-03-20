f4de75d53188f805b8641bf64892b458486ced7c
oma16 <48384861+oma16@users.noreply.github.com>
2021-03-20 01:28:26 +0100

    Update mysql-connect.php

--- mysql-connect.php
+++ mysql-connect.php
@@ -14,5 +14,30 @@ if(!$dbc){
     echo '';
 }
 
+$active_group ='default';
+$query_builder =TRUE;
+$db['default'] =  array(
+'dsn' => '';
+'hostname' => 'eu-cdbr-west-03.cleardb.net';
+'username' => 'b7288f43bb52cf';
+'password' => '806062fb';
+'database' => 'heroku_c85d074a1a36290';
+'dbdriver' => 'mysqli';
+'dbprefix' => '';
+'pconnect' =>FALSE;
+'db_debug' => (ENVIRONMENT !== 'production');
+'cache_on' => FALSE;
+'cachedir' => '';
+'char_set' => 'utf8';
+'dbcollat' => 'utf8_general_ci';
+'swap_pre' => '';
+'encrypt' => FALSE;
+'compress' => FALSE;
+'stricton' => FALSE;
+'failover' => array();
+'save_queries' => TRUE;
 
+
+
+);
 ?>
